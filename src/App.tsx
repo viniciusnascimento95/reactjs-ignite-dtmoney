47d3c4a60a602079c9018e2ba8ec33e143dec1b9
vinicius.souza <vinicius.souza@smartsupport.com.br>
2021-09-18 15:46:17 -0300

    update project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard/>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
