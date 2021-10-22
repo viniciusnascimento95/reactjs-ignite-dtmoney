a17e0466251805237256f2930f615910d09a1490
souza <vinicius.souza@smartsupport.com.br>
2021-10-22 10:04:35 -0300

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './hooks/useTransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -19,7 +20,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
 
@@ -29,7 +30,7 @@ export function App() {
 
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
